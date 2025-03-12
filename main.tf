81f48342e3f534715c800d8d08221e7b54077f0a
sio1212 <77596052+sio1212@users.noreply.github.com>
2025-03-12 15:30:12 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-back-gt"  # S3 버킷 이름
+    key    = "terraform.tfstate"            # 상태 파일이 저장될 경로
+    region = "ap-northeast-2"                    # AWS 리전
+    encrypt = true                          # 상태 파일 암호화
+ #   dynamodb_table = "terraform-locks"      # 상태 파일 잠금을 위한 DynamoDB 테이블 (선택 사항)
+  }
+}
+
+# VPC 생성 예시
 provider "aws" {
-  region = "ap-northeast-2"  # 원하는 AWS 리전으로 수정
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"  # 원하는 CIDR 블록으로 수정
+  cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
     Name = "MyVPC"
   }
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
 }
