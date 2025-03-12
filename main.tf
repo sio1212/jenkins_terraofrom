20d1fd93e1960387804b74acfe999e0945313166
sio1212 <77596052+sio1212@users.noreply.github.com>
2025-03-12 15:16:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
-#### VPC ####
+provider "aws" {
+  region = "ap-northeast-2"  # 원하는 AWS 리전으로 수정
+}
 
-resource "aws_vpc" "vpc-test-jenkins" {
-  cidr_block  = "10.181.130.0/24" # 업뎃 필요
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"  # 원하는 CIDR 블록으로 수정
+  enable_dns_support = true
   enable_dns_hostnames = true
-
   tags = {
-    Name = "vpc-test-jenkins"
+    Name = "MyVPC"
   }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
