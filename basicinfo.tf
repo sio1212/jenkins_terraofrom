6c3cc74b68288f7d8eb8b06061612a01e4da132e
sio1212 <77596052+sio1212@users.noreply.github.com>
2025-03-24 16:08:52 +0900

    Update basicinfo.tf

--- basicinfo.tf
+++ basicinfo.tf
@@ -20,15 +20,7 @@ terraform {
   # Terraform 버전
   required_version = ">= 0.13"
 
-  # State 저장용 S3 Backend 구성
-  #backend "s3" {
-  #  bucket = "" #S3 버킷명
-  #  key    = "" #terraform.tfstate 파일위치 ex) aws-an2/jenkins_terraform_test/terraform.tfstate
-  #  region = "ap-northeast-2" # 리전
-  #  dynamodb_table = "" #테라폼 백엔드 구성용 DynamoDB 테이블명
-  #  encrypt = true
-  #}
-#}
+  }
 
 #### Remote State Data Source ####
 #data "aws_availability_zones" "all" {}
