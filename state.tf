b286a3ed65bf5e679a16cde9d91779c6ef14ad0a
sio1212 <77596052+sio1212@users.noreply.github.com>
2025-03-24 16:03:33 +0900

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,4 +1,4 @@
-# Terraform의 백엔드 설정 (상태 파일을 저장할 위치 및 방법을 지정)
+ # Terraform의 백엔드 설정 (상태 파일을 저장할 위치 및 방법을 지정)
 terraform {
   backend "s3" {
     # S3 버킷 이름 - 이 버킷에 Terraform 상태 파일이 저장됩니다.
@@ -18,15 +18,6 @@ terraform {
   }
 }
 
-# AWS Provider 설정 (Terraform에서 AWS와 상호작용을 위한 설정)
-provider "aws" {
-  region = "ap-northeast-2"  # AWS 리전 설정 (여기서는 서울 리전)
-}
 
 # 예시로 사용할 리소스 (AWS S3 버킷 생성)
-resource "aws_s3_bucket" "jgt-terraform-state" {
-  bucket = "jgt-terraform-state"  # S3 버킷 이름 (기존에 만든 이름 사용)
-  acl    = "private"  # 접근 제어 목록 (ACL) 설정, private으로 설정하여 모든 사람이 접근하지 못하도록 설정
-}
-
 # 기타 추가 리소스를 여기에 정의할 수 있습니다.
