d5a94290138e68f5974b24b3751a904c03c2a013
sio1212 <77596052+sio1212@users.noreply.github.com>
2025-03-28 10:35:20 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ resource "aws_vpc" "vpc-test-jenkins" {
   cidr_block  = "10.181.130.0/24" # 업뎃 필요
   enable_dns_hostnames = true
   enable_dns_support = true
+ 
+  enable_classiclink           = false  # 필요한 경우 true/false로 설정
+  enable_classiclink_dns_support = false  # 필요한 경우 true/false로 설정
   tags = {
     Name = "vpc-test-jenkins"
   }
