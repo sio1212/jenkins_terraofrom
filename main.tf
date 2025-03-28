d95560c1f00e644b3cc331e17d99026ab23c0e8b
sio1212 <77596052+sio1212@users.noreply.github.com>
2025-03-28 10:28:14 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 resource "aws_vpc" "vpc-test-jenkins" {
   cidr_block  = "10.181.130.0/24" # 업뎃 필요
   enable_dns_hostnames = true
-
+  enable_dns_support = true
   tags = {
     Name = "vpc-test-jenkins"
   }
